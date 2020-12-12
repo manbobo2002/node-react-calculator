c73087ddf07590d0ce6ded12a511a8a260508b37
MPL <manbobo2012@gmail.com>
2020-12-12 21:09:11 +0800

    final

--- client/src/App.js
+++ client/src/App.js
@@ -17,9 +17,8 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React 3
           </a>
-          <h1 className="App-title">Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
