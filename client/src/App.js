c89831aceff67c88c7c3875a552ff0c9ea914e89
MPL <manbobo2012@gmail.com>
2020-12-13 16:58:14 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './calculator.png';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
@@ -10,15 +10,8 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 3
-          </a>
+          <img src={logo} alt="logo" width="10%" height="10%"/>
+            Here is Fib calculator
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
